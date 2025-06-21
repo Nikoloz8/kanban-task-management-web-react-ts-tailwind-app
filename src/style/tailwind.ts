51407ce07926a0f8790dc504f55a7913433834f0
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-21 19:06:45 +0400

    create styles folder and write heading styles in tailwind.ts file

---
+++ src/style/tailwind.ts
@@ -0,0 +1,11 @@
+export default function tailwind() {
+
+    const H1 = "text-[2.4rem] font-[700] text-[#000112] leading-[100%]"
+    const H2 = "text-[1.8rem] font-[700] text-[#000112] leading-[100%]"
+    const H3 = "text-[1.5rem] font-[700] text-[#000112] leading-[100%]"
+    const H4 = "text-[1.2rem] font-[700] text-[#828FA3] leading-[100%] tracking-[2.4px]"
+    const P1 = "text-[1.3rem] font-[500] text-[#2B2C37] leading-[23px]"
+    const P2 = "text-[1.2rem] font-[700] text-[#2B2C37] leading-[100%]"
+
+    return { H1, H2, H3, H4, P1, P2 }
+}
