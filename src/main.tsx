f36773b8084777e692e5aee23898908cb5ef895d
NikolozTevdoradze <nikoloztevdoradze08@gmail.com>
2025-06-21 23:18:14 +0400

    create Board page in pages and change router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import Board from './pages/Board'
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
-    path: "/"
+    path: "/",
+    children: [
+      {
+        element: <Board />,
+        path: "/:board"
+      }
+    ]
   }
 ])
 
